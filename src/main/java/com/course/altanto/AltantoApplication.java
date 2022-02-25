f2b387938db0f0d787a20521023e09ce83605482
Gabriel Juarez De La Cruz <42722962+gajuarezdelac@users.noreply.github.com>
2022-02-25 10:55:18 -0600

    AP

--- src/main/java/com/course/altanto/AltantoApplication.java
+++ src/main/java/com/course/altanto/AltantoApplication.java
@@ -28,7 +28,7 @@ public class AltantoApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://gajuarezdelac.github.io"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
